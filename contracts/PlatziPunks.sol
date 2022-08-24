f1c0a59743c56ad315329570ce2cd8a570d82d3f
Eimer Castro <Eimer.Castro@nordstrom.com>
2022-08-24 08:42:59 -0500

    Extending functionality to mint tokens based on a max supply and letting opened to interact with a Frontend.

--- contracts/PlatziPunks.sol
+++ contracts/PlatziPunks.sol
@@ -2,8 +2,40 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract PlatziPunks is ERC721 {
+contract PlatziPunks is ERC721, ERC721Enumerable {
+    using Counters for Counters.Counter;
 
-    constructor() ERC721("PlatziPunks", "PLPKS") {}
+    Counters.Counter private _tokenIdCounter;
+    uint256 public maxSupply;
+
+    constructor(uint256 _maxSupply) ERC721("PlatziPunks", "PLPKS") {
+        maxSupply = _maxSupply;
+    }
+
+    function safeMint() public {
+        uint256 currentTokenId = _tokenIdCounter.current();
+        require(currentTokenId < maxSupply, "No PlatziPunks left :(");
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, currentTokenId);
+    }
+
+    // Override required
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
