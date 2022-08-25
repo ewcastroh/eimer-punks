e47ee222e2adfd29efd7f4b3a154be7332812321
Eimer Castro <Eimer.Castro@nordstrom.com>
2022-08-24 20:35:21 -0500

    Implementing tokenURI method to build metadata info.

--- contracts/PlatziPunks.sol
+++ contracts/PlatziPunks.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
 contract PlatziPunks is ERC721, ERC721Enumerable {
     using Counters for Counters.Counter;
@@ -22,11 +23,33 @@ contract PlatziPunks is ERC721, ERC721Enumerable {
         _safeMint(msg.sender, currentTokenId);
     }
 
-    // Override required
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable)
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
     {
+        require(
+            _exists(_tokenId),
+            "ERC721 Metadata: URI query for nonexisting token."
+        );
+        string memory jsonURI = Base64.encode(
+            abi.encodePacked(
+                '{ "name": "PlatziPunks #', _tokenId, '"',
+                '"description:": "Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi"',
+                '"image": "TODO: Calculate image URL"',
+                '}'
+                )
+            );
+        return string(abi.encodePacked("data:application/json;base64", jsonURI));
+    }
+
+    // Override required
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
